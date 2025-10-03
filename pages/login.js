bb012dd526f9a10848dbf344057754839a701042
Lubis Karisma Ariansyah <72157953+arnshyz@users.noreply.github.com>
2025-10-03 21:47:02 +0700

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -189,7 +189,7 @@ export default function Login() {
             className="mb-3 w-full rounded-lg border border-white/10 bg-white/10 px-3 py-2 text-white placeholder:text-neutral-400 outline-none focus:ring-2 focus:ring-white/20"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
-            placeholder="admin"
+            placeholder="username"
           />
 
           <label className="mb-1 block text-sm text-neutral-200">Password</label>
