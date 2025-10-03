37e7dd878ef35abccb2365c6b50a82eb109cf976
Lubis Karisma Ariansyah <72157953+arnshyz@users.noreply.github.com>
2025-10-03 08:48:54 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+
+export default function IndexPage() {
+  return <main style={{padding:20}}>Dashboard OK</main>
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ["./pages/**/*.{js,jsx,ts,tsx}"],
+  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}", "./app/**/*.{js,jsx}"],
   theme: { extend: {} },
   plugins: [],
 };
